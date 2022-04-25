9bc725a3935e58ada4b54b0f93f92b4cbd02c87e
Pavel Filonov <qqq.qqq48@yandex.ru>
2022-04-25 15:54:34 +0300

    Initial commit

---
+++ webApi/src/main/java/com/edu/moneywayapi/webApi/MoneyWayApiApplication.java
@@ -0,0 +1,13 @@
+package com.edu.moneywayapi.webApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyWayApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyWayApiApplication.class, args);
+    }
+
+}
